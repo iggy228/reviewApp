7ad77f470c985b879e63c832dc65b76df518511a
iggy228 <i.borovy@gmail.com>
2020-10-28 18:39:00 +0100

    preparing structures and fonts.

---
+++ screens/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+export default function Home() {
+    return (
+        <View >
+            <Text>About screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+})
---
+++ screens/ReviewDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+export default function Home() {
+    return (
+        <View >
+            <Text>Review screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+})
