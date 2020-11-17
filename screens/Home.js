c563aa545d93e38519a255dde7a88ddc816a224b
iggy228 <i.borovy@gmail.com>
2020-11-17 11:34:10 +0100

    Making connection to review API.

--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, FlatList, Text, TouchableOpacity } from 'react-native';
 
 import { globalStyles } from '../styles/global'
 import Card from '../components/Card'
 
 export default function Home({ navigation }) {
-    const [review, setReview] = useState([
-        {title: 'Zelda', rating: 5, body: 'Lorem ipsum sit dolor', key: '1'},
-        {title: 'Pokemon red and white', rating: 4, body: 'Lorem ipsum sit dolor', key: '2'},
-        {title: 'Not so "Final Fantasy"', rating: 2, body: 'Lorem ipsum sit dolor', key: '3'},
-    ])
+    const [review, setReview] = useState({})
+
+    useEffect(() => {
+        fetch('http://192.168.241.238:3000').then(res => res.json()).then(data => {
+            setReview(data.reviews)
+        })
+    })
 
     return (
         <View>
             <FlatList
-                key={review.key}
+                keyExtractor={item => item.id}
                 data={review} 
                 renderItem={
                 ({ item }) => (
